1e719d97b33b020d6708740d8fc38d663b98fef8
Lautaro Vallejos <lautaro.vallejosk4m@gmail.com>
2022-08-29 15:59:55 -0300

    Actividad del 26 de Agosto finalizada

--- Agosto26.js
+++ Agosto26.js
@@ -1,32 +1,49 @@
-// Validador de gmail
+// Programa para añadir usuarios y contraseñas utilizando metodos de manipualcion de strings
 
-var validated = false
+var noValidated = false
 var users = []
 var passwords = []
 do {
-    //Solicita Email
+    //Solicita y valida el mail
     let Email = prompt("Ingrese su mail:")
+    if(Email.includes("@gmail.com")){
 
-    //Valida el email
-    if(Email.includes("@gmail") && Email.includes(".com")){
-
-        //Solicita y Valida la contra
+        //Solicita y valida la contraseña
         let Pass = prompt("Ingrese su contraseña, (debe contener un signo de $)")
         if(Pass.includes("$")){
+
             // Consulta si quiere ingresar otro
             alert("Usuario ingresado satisfactoriamente")
             back = prompt("Desea ingresar otro usuario? (Y/N)")
             if(back.toLowerCase() == "y"){
-                validated = false
+                // El bucle se repite y envia los datos al array
+                noValidated = false
+                users.push(Email)
+                passwords.push(Pass)
                 
             } else {
-                validated = true
-                
+                // El bucle se termina y envia los datos al array
+                noValidated = true
+                users.push(Email)
+                passwords.push(Pass)
             }
         } else{
             alert("La contraseña no cumple con los requisitos")
         }
+
     } else{
         alert("Email invalido, volve a intentar")
     }
-} while (validated != true);
+
+} while(noValidated != true);
+
+//Retorna los datos para el output
+getAllUsers = users.map(user => user)
+getAllPass = passwords.map(pass => pass)
+
+// Fin del programa y datos por pantalla
+alert("### Fin del Programa | Resultados en la Consola ###")
+console.log("Listado de Usuarios")
+console.log(getAllUsers)
+console.log("Listado de Contraseñas")
+console.log(getAllPass)
